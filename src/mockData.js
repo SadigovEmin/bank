a2d79f467469fe12a3a3979bad91febd51591b8a
SadigovEmin <2eminsadiqov@gmail.com>
2026-06-19 12:56:09 +0400

    implement initial file and folder structure

---
+++ src/mockData.js
@@ -0,0 +1,13 @@
+export const mockUser = {
+    name: "Emin",
+    accountNumber: "9999 9999 1111 1111",
+    balance: 4800,
+    currency: "USD"
+}
+
+export const mockTransaction = [
+    {id: 1, type: "expense", category:"Electronics", amount: 1800, date: "2026-02-18", merchant:"Azerbaijan Electronics" },
+    {id: 2, type: "expense", category:"Subscription", amount: 6.50, date: "2026-06-20", merchant:"Spotify" },
+    {id: 3, type: "income", category:"Salary", amount: 2500, date: "2026-07-26", merchant:"AZE Bank" }
+
+]
